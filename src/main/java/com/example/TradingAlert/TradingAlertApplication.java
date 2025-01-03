0db5a13893de313224d5931761fcc186325088f1
dmsdms39 <dmsdms@cau.ac.kr>
2025-01-03 20:10:34 +0900

    init project

---
+++ src/main/java/com/example/TradingAlert/TradingAlertApplication.java
@@ -0,0 +1,13 @@
+package com.example.TradingAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAlertApplication.class, args);
+	}
+
+}
