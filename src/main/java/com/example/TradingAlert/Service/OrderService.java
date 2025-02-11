8bcac3193a8374f2e9ffbcebfe96c5161e32a50a
dmsdms39 <dmsdms@cau.ac.kr>
2025-02-11 17:55:42 +0900

    objectMapper 제거 -> jsonMessageConverter 사용

--- src/main/java/com/example/TradingAlert/Service/OrderService.java
+++ src/main/java/com/example/TradingAlert/Service/OrderService.java
@@ -1,8 +1,6 @@
 package com.example.TradingAlert.Service;
 
 import com.example.TradingAlert.Dto.TradeStock;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,19 +11,12 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final RabbitTemplate rabbitTemplate;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Value("${spring.rabbitmq.queue.name}")
+    @Value("${spring.rabbitmq.queue.order}")
     private String queueName;
 
     public void sendOrder(TradeStock order) {
-        try {
+            rabbitTemplate.convertAndSend(queueName, order);
             System.out.println(" [OrderService] Sent Order: " + order);
-            String jsonOrder = objectMapper.writeValueAsString(order);
-            rabbitTemplate.convertAndSend(queueName, jsonOrder);
-            System.out.println(" [OrderService] Sent Order: " + jsonOrder);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/main/java/com/example/TradingAlert/Service/RabbitMQListener.java
+++ src/main/java/com/example/TradingAlert/Service/RabbitMQListener.java
@@ -1,5 +1,6 @@
 package com.example.TradingAlert.Service;
 
+import com.example.TradingAlert.Dto.TradeStock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ class RabbitMQListener {
     private final TradingService  tradingService;
 
     @RabbitListener(queues = "order_queue")
-    public void receiveMessage(String message) {
+    public void receiveMessage(TradeStock message) {
         System.out.println(" [RabbitMQListener] Received message: " + message);
         tradingService.processOrder(message);
     }
